4937afabf0edb48bbe67b87d5d47937521fecb11
SaiTeja <saitejasuperpower123@gmail.com>
2024-12-06 07:54:44 +0530

    getting cloudinary storage issue

--- Major Project/routes/listings.js
+++ Major Project/routes/listings.js
@@ -3,7 +3,8 @@ import wrapAsync from '../utils/wrapAsyncError.js';
 import middlewares from '../middleware.js';
 import listingController from "../controller/listings.js";
 import multer from 'multer';
-const upload = multer({dest : 'uploads/'})
+import {storage} from '../cloudinaryConfig.js'
+const upload = multer({storage})
 
 const {listingValidation , isLoggedIn, isOwner } = middlewares;
 const {allListings , getSingleListing , getNewListting, postaddnewListing , getEditListing , putEditNewListing , destroyListing , destroyAllListing} = listingController;
