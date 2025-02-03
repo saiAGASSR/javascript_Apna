8b01033e286dd47a9295de743b7bd549064dec1d
saiAGASSR <146331682+saiAGASSR@users.noreply.github.com>
2025-02-03 23:47:54 +0530

    react Document

--- Major Project/utils/customError.js
+++
@@ -1,9 +0,0 @@
-class customError extends Error {
-    constructor(status,message){
-        super();
-        this.statusCode  = status;
-        this.message = message;
-    }
-}
-
-export default customError;
